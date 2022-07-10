7b293a6fb20ed6d93eac20dfcdea1b33732aeca1
Arias-unlam <milarias@alumno.unlam.edu.ar>
2022-07-10 16:00:35 -0300

    Inicio del proyecto

---
+++ src/main/java/com/macons/apparkme/ApparkmeApplication.java
@@ -0,0 +1,13 @@
+package com.macons.apparkme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparkmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApparkmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macons/apparkme/ApparkmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macons.apparkme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApparkmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
