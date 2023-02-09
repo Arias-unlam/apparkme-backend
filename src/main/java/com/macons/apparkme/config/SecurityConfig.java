562e49a617fded9cecde2949f770b450d311f700
bruno4ri4s <milarias@alumno.unlam.edu.ar>
2023-02-09 00:36:33 -0300

    Se agrega mapper y se crea el service para la generacion del token

--- src/main/java/com/macons/apparkme/config/SecurityConfig.java
+++ src/main/java/com/macons/apparkme/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.macons.apparkme.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +14,44 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
         jsr250Enabled = true,
         prePostEnabled = true
 )
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().
+                antMatchers("/**")
+                .permitAll().anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable();
+
+        /*http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE)
+                .hasRole("ADMIN")
+                .antMatchers("/admin/**")
+                .hasAnyRole("ADMIN")
+                .antMatchers("/user/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/login/**")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
+
+        return http.build();
+    }
+
+    /*protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated().and().csrf().disable();
-    }
+    }*/
 
 }
